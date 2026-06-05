4b95c8ef5168fbdf1aa4d3d7c1d684387b555240
hugoalcala <hugo.pascual@live.u-tad.com>
2026-06-05 09:27:54 +0200

    fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -324,8 +324,6 @@ class AuthController extends Controller
             'redirect_uri' => $redirectUri,
             'response_type' => 'code',
             'scope' => implode(' ', $scopes),
-            'access_type' => 'offline',
-            'prompt' => 'consent',
         ]);
 
         return response()->json([
